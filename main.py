e4991df558c759f1c129d09775f5ba4abb84558f
MaxSSD <86234226+MaxSSD@users.noreply.github.com>
2021-11-22 08:30:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,24 +16,27 @@ ageModel = "age_net.caffemodel"
 genderProto = "gender_deploy.prototxt"
 genderModel = "gender_net.caffemodel"
 
+# Rules for identyfying age
 faceNet=cv2.dnn.readNet(faceModel, faceProto)
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
 genderNet=cv2.dnn.readNet(genderModel,genderProto)
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+ageList = ['(0-3)', '(4-7)', '(8-14)', '(15-24)', '(25-37)', '(38-47)', '(48-59)', '(60-100)']
 genderList = ['Male', 'Female']
 padding=20
 
 """ Face highliting """
 
 def faceBox(faceNet, frames):
+    # Set rules of highlighting
     frameHeight=frames.shape[0]
     frameWidth=frames.shape[1]
     blob=cv2.dnn.blobFromImage(frames, 1.0, (300,300), [104,117,123], swapRB=False)
     faceNet.setInput(blob)
     detection=faceNet.forward()
     bboxs=[]
+    # Set the grid
     for i in range(detection.shape[2]):
         confidence=detection[0,0,i,2]
         if confidence>0.7:
@@ -50,7 +53,7 @@ def faceBox(faceNet, frames):
 def DisplayVid():
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('testvideo', fourcc, 20.0, (640, 480))
+    out = cv2.VideoWriter('output.avi', fourcc, 60.0, (640, 480))
 
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
@@ -77,7 +80,7 @@ def DisplayVid():
             cv2.rectangle(frameFace, (bbox[0], bbox[1] - 30), (bbox[2], bbox[1]), (0, 255, 0), -1)
             cv2.putText(frameFace, label, (bbox[0], bbox[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2,
                         cv2.LINE_AA)
-        cv2.imshow("Age-Gender", frameFace)
+        cv2.imshow("age-gender", frameFace)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
